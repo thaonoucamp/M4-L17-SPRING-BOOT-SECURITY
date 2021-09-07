cc21ed2c3dcf2a2becd3e48359956d8d66c07803
thaonoucamp <dangxuanthaoht@gmail.com>
2021-09-07 11:31:47 +0700

    boot

---
+++ src/main/java/com/codegym/demospringbootsecurity/DemoSpringBootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.demospringbootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/demospringbootsecurity/DemoSpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.demospringbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
